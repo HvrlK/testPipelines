beb26093baefdc883a8ebaac9cee144e7039d061
Vitalii Havryliuk <v.havryliuk@noisyminer.com>
2020-06-29 15:24:14 +0300

    Add port

--- application.go
+++ application.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
 	r := gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"success": false,
 		})
 	})
-	r.Run()
+	r.Run(":" + port)
 }
